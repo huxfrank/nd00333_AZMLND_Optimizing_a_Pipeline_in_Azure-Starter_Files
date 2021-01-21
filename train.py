c19dae8cfe26b4b5994fdb8e17c3d96dc1f930ce
huxfrank <xxh75@case.edu>
2021-01-21 05:11:58 +0000

    both things work now
    need to write readme and clean up files

--- train.py
+++ train.py
@@ -87,5 +87,8 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(LogisticRegression, 'outputs/model.joblib')
+
 if __name__ == '__main__':
     main()
