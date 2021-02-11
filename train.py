ea8af3f536c8d79f275e16e6a39687b0598ecbbc
Frank <xfrank.hu@gmail.com>
2021-02-10 19:10:52 -0800

    cleaned up code and readme

--- train.py
+++ train.py
@@ -11,11 +11,9 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 def clean_data(data):
-    # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
 
-    # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     jobs = pd.get_dummies(x_df.job, prefix="job")
     x_df.drop("job", inplace=True, axis=1)
@@ -38,31 +36,18 @@ def clean_data(data):
 
     return x_df,y_df
     
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
+#Create / Clean Dataset
 web_path = ['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv']
 ds = TabularDatasetFactory.from_delimited_files(path=web_path)
-#print(ds.to_pandas_dataframe().dropna())
-
-# TODO: Split data into train and test sets.
-# train and test splits output
 
 x , y = clean_data(ds)
 x_train, x_test, y_train, y_test = train_test_split(x,y)
 
+#Run
 run = Run.get_context()
 
 def main():
-    # Add arguments to script
-    #parser = argparse.ArgumentParser()
-
-    #parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    #parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
-    #args = parser.parse_args()
-
+    #Logging pertinent information and metrics
     run.log("Regularization Strength:", np.float(1.0))
     run.log("Max iterations:", np.int(100))
 
